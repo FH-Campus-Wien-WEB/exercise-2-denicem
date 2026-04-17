36fc141aed3adb4d44ece86c35b99b0e1be8afa8
Denice M <denicem@hotmail.de>
2026-04-17 22:52:19 +0200

    add /movies endpoint

--- server/files/index.js
+++ server/files/index.js
@@ -1,24 +1,119 @@
 window.onload = function () {
-  const xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    const bodyElement = document.querySelector("body");
-    if (xhr.status == 200) {
-      const movies = JSON.parse(xhr.responseText);
-      for (const movie of movies) {
-        /* Task 1.3. Add your code from exercise 1 here 
-           and include a non-functional 'Edit' button
-           to pass this test */
-      }
+	const xhr = new XMLHttpRequest();
+	xhr.onload = function () {
+		const bodyElement = document.querySelector("body");
+		if (xhr.status == 200) {
+			const movies = JSON.parse(xhr.responseText);
+			const fragment = document.createDocumentFragment();
+			for (const movieId of movies) {
+				/* Task 1.3. Add your code from exercise 1 here 
+				and include a non-functional 'Edit' button
+				to pass this test */
+				const movie = Object.values(movieId)[0];
+				const movieArticle = document.createElement('article');
 
-    } else {
-      bodyElement.append(
-        "Daten konnten nicht geladen werden, Status " +
-          xhr.status +
-          " - " +
-          xhr.statusText
-      );
-    }
-  };
-  xhr.open("GET", "/movies");
-  xhr.send();
+				const movieOverview = document.createElement('div');
+				movieOverview.classList.add('movie-overview');
+
+				// extracting poster img
+				const moviePoster = document.createElement('img');
+				moviePoster.src = movie.Poster;
+
+				// extracting movie title
+				const movieTitle = document.createElement('h1');
+				movieTitle.textContent = movie.Title;
+
+				// extracting movie info (Runtime, Released)
+				const movieInfo = document.createElement('p');
+				let runtimeMinutes = movie.Runtime;
+				let runtimeHours = 0;
+				while (runtimeMinutes >= 60) {
+					++runtimeHours;
+					runtimeMinutes -= 60;
+				}
+				const movieReleased = movie.Released.split('T')[0].split('-').reverse().join('/');
+				movieInfo.textContent = `Runtime ${runtimeHours}h ${runtimeMinutes}m•Released on  ${movieReleased}`;
+
+				// extracting Genres
+				const movieGenres = document.createElement('p');
+				movie.Genres.forEach(genre => {
+					const genreSpan = document.createElement('span');
+					genreSpan.classList.add('genre');
+					genreSpan.textContent = genre;
+					movieGenres.append(genreSpan);
+				});
+
+				// extracting Plot
+				const moviePlot = document.createElement('p');
+				moviePlot.textContent = movie.Plot;
+
+				// extracting ratings
+				const ratingsContainer = document.createElement('div');
+				ratingsContainer.classList.add('ratings-box');
+
+				const imdbScore = document.createElement('span');
+				imdbScore.textContent = `IMDb: ${movie.imdbRating}/10 ⭐️`;
+
+				const metaScore = document.createElement('span');
+				metaScore.textContent = `Metascore: ${movie.Metascore} 🍅`;
+				
+				ratingsContainer.append(imdbScore, " | ", metaScore);
+				movieOverview.append(moviePoster, movieTitle, movieInfo, movieGenres, moviePlot, ratingsContainer);
+
+				// extracting cast and crew
+				const castAndCrew = document.createElement('div');
+				castAndCrew.classList.add('cast-and-crew');
+				
+				// extracting Directors
+				const directorTitle = document.createElement('h2');
+				directorTitle.textContent = "Director";
+				const directorArray = movie.Directors;
+				const directorList = document.createElement('ul');
+				directorArray.forEach(director => {
+					const directorElement = document.createElement('li');
+					directorElement.textContent = director;
+					directorList.append(directorElement);
+				});
+				castAndCrew.append(directorTitle, directorList);
+
+				// extracting Writers
+				const writerTitle = document.createElement('h2');
+				writerTitle.textContent = "Writers";
+				const writerArray = movie.Writers;
+				const writerList = document.createElement('ul');
+				writerArray.forEach(writer => {
+					const writerElement = document.createElement('li');
+					writerElement.textContent = writer;
+					writerList.append(writerElement);
+				});
+				castAndCrew.append(writerTitle, writerList);
+
+				// extracting Actors
+				const actorTitle = document.createElement('h2');
+				actorTitle.textContent = "Actors";
+				const actorArray = movie.Actors;
+				const actorList = document.createElement('ul');
+				actorArray.forEach(actor => {
+					const actorElement = document.createElement('li');
+					actorElement.textContent = actor;
+					actorList.append(actorElement);
+				});
+				castAndCrew.append(actorTitle, actorList);
+
+				movieArticle.append(movieOverview, castAndCrew);
+				fragment.append(movieArticle);
+			}
+			bodyElement.append(fragment);
+		} 
+		else {
+			bodyElement.append(
+				"Daten konnten nicht geladen werden, Status " +
+				xhr.status +
+				" - " +
+				xhr.statusText
+			);
+		}
+	};
+	xhr.open("GET", "/movies");
+	xhr.send();
 };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const movieModel = require('./movie-model.js');
 
 const app = express();
+let movies = [];
 
 // Parse urlencoded bodies
 app.use(bodyParser.json()); 
@@ -11,26 +12,31 @@ app.use(bodyParser.json());
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
+
 // Configure a 'get' endpoint for all movies..
 app.get('/movies', function (req, res) {
-  /* Task 1.2. Remove the line below and eturn the movies from 
-     the model as an array */
-  res.sendStatus(404)
+	/* Task 1.2. Remove the line below and return the movies from the model as an array */
+	console.log("ENDPOINT '/movies' called.");
+	res.json(movies);
 })
 
 // Configure a 'get' endpoint for a specific movie
 app.get('/movies/:imdbID', function (req, res) {
-  /* Task 2.1. Remove the line below and add the 
-    functionality here */
-  res.sendStatus(404)
+	/* Task 2.1. Remove the line below and add the functionality here */
+	res.sendStatus(404)
 })
 
 /* Task 3.1 and 3.2.
-   - Add a new PUT endpoint
-   - Check whether the movie sent by the client already exists 
-     and continue as described in the assignment */
+	- Add a new PUT endpoint
+	- Check whether the movie sent by the client already exists 
+	 and continue as described in the assignment */
 
-app.listen(3000)
+//	initializing data and server
 
-console.log("Server now listening on http://localhost:3000/")
-
+movieModel.initMovies().then(data => {
+	movies = data;
+	console.log(`Successfully loaded ${movies.length} movies.`);
+	app.listen(3000, () => {
+		console.log("Server now listening on http://localhost:3000/");
+	});
+});
